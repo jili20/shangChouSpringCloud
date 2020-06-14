8ff434bcb33a59d938c3fbca6b3d95b2e61ef298
jili20 <bing_yu2001@163.com>
2020-06-15 00:23:23 +0800

    实现集群轮询负载均衡

--- pro08-spring-cloud-provider/src/main/java/com/atguigu/spring/cloud/handler/EmployeeHandler.java
+++ pro08-spring-cloud-provider/src/main/java/com/atguigu/spring/cloud/handler/EmployeeHandler.java
@@ -1,5 +1,7 @@
 package com.atguigu.spring.cloud.handler;
 
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -9,7 +11,9 @@ import com.atguigu.spring.cloud.entity.Employee;
 public class EmployeeHandler {
 	
 	@RequestMapping("/provider/get/employee/remote")
-	public Employee getEmployeeRemote() {
-		return new Employee(555, "tom555", 5555.55);
+	public Employee getEmployeeRemote(HttpServletRequest request) {
+		// 获取当前服务的端口号
+		int serverPort = request.getServerPort();
+		return new Employee(555, "tom555" + serverPort, 5555.55);
 	}
 }
